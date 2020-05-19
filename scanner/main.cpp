7842f52ed192b50b35ddd91e6f4f2d9cb3cd5fac
mohammadrezaabdi <reza_abdi20@yahoo.com>
2020-05-19 14:01:30 +0430

    fixing bash scripts

---
+++ scanner/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    if (argc < 5) {
+        cerr << "Usage: " << argv[0] << " -i <input> -o <output>" << endl;
+        return 1;
+    }
+    string input_file_name = argv[2];
+    string output_file_name = argv[4];
+
+    system("gcc -o a.out lex.yy.c");
+//    string run = "./a.exe " + input_file_name +" out/"+ output_file_name;
+    string run = "./a.out tests/" + input_file_name + " out/" + output_file_name;
+    system(run.c_str());
+}
