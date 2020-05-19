71e84a57a408da80f51057cfa3d603964afc7857
mohammadrezaabdi <reza_abdi20@yahoo.com>
2020-05-19 13:51:18 +0430

    fixing scanner 1 : removing data

---
+++ scanner/main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by nick white on 5/19/2020.
+//
+
+#include "main.h"
---
+++ scanner/main.h
@@ -0,0 +1,16 @@
+//
+// Created by nick white on 5/19/2020.
+//
+
+#ifndef SCANNER_MAIN_H
+#define SCANNER_MAIN_H
+
+
+
+class main {
+
+};
+
+
+
+#endif //SCANNER_MAIN_H
